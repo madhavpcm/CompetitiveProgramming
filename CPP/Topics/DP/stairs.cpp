922b8ebe12704cfa2cb9ca5de6def0079e92d7b1
madhavpcm <dpmadhav@hotmail.com>
2021-01-04 08:46:19 +0530

    Templates folder added to use alongside custom script, other problems added

--- CPP/Topics/DP/stairs.cpp
+++ CPP/Topics/DP/stairs.cpp
@@ -1,28 +1,47 @@
 #include<iostream>
 #include<stdint.h>
 #include<vector>
+#include<chrono>
+
 using namespace std;
 
-#define MOD = 10e9 + 7;
-vector<int32_t> dp(100001,0);
+long long MOD= 10e9 + 7;
+vector<long long> dp(1000001,0);
 
-int solve(int n ){
-    dp[1] = 1;
+int solve(int32_t n ){
+    return dp[n];
+}
+long long recursive_solve( const int n){
+    if (n<=0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    if (n == 3) return 4;//base cases-
+    
+    if (dp[n] != 0) { return dp[n]; }//checking if already solved
+    
+    dp[n] = recursive_solve( n-1) + recursive_solve( n-2) + recursive_solve(n-3);
+
+    return dp[n];
+}
+int main(){
+    auto start = chrono::high_resolution_clock::now();
+    int32_t t =0;cin>>t;
+    int32_t n =0;
+    
+   /* dp[1] = 1;
     dp[2] = 2;
     dp[3] = 4;
 
     for( int i = 4 ; i <1000001; i++ ){
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        
-    }
-}
-int main(){
-    int32_t t =0;cin>>t;
-    int32_t n =0;cin>>n;
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % MOD;
+    }*/
 
     while(t--){
-        cout<<solve(n);
+        cin>>n;    
+        cout<<recursive_solve(n)<<endl;
     }
-
+    auto end = chrono::high_resolution_clock::now();
+    auto dur = chrono::duration<float , milli>(end-start);
+    cout<<dur.count()<<endl;
     return 0 ;
 }
